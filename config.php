dd477266db17ab6098c7afe96f7d6f64062b68aa
hoaian33333 <nguyenangaming61@gmail.com>
2025-12-16 16:58:11 +0700

    PHP giao diện

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'onehost-wphn092505.000nethost.com');
-define('DB_USER', 'cidkghirhosting_lumos');
-define('DB_PASS', '.1S#$SdUjs&BMaW');
-define('DB_NAME', 'cidkghirhosting_csdl_lumos');
+define('DB_USER', 'hwsqelrphosting_hoaian1111');
+define('DB_PASS', '>J6*[ZhlumJQ!E9');
+define('DB_NAME', 'hwsqelrphosting_phpdata');
 define('DB_PORT', 3306);
